016b77cc142b019c1e8692e8a083d97f84f34776
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2025-08-24 18:14:23 +0300

    + changed title in appbar.

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.orange,
-          title: Text('Test'),
+          title: Text('Git Test'),
         ),
       ),
     );
